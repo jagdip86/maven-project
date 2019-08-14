18ac738fdb789821d76f587c59b24594f57b8e93
Singh <jagdipsin@basware.com>
2019-08-14 14:20:09 +0530

    last check

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -11,7 +11,11 @@ public class Greeter {
   public Greeter() {
 
   }
-
+  /**
+   * 
+   * @param someone the name
+   * @return greetings
+   */
   public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
   }
